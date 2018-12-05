e2ec2b69f3797f9dc2bc5a72ca4087fddd0a8d78
Zireael07 <zi@mail.com>
2018-12-05 16:21:45 +0100

    new: 2d straights and curves that allow specifying width and draw using polygons

---
+++ curve_better.gd
@@ -0,0 +1,61 @@
+tool
+extends "curve.gd"
+
+# class member variables go here, for example:
+export(int) var width = 40
+var inner_arc = []
+var outer_arc = []
+
+func _ready():
+	print("Curve ready")
+	thick = 1
+	
+	# arc
+	inner_arc = get_circle_arc(Vector2(0,0), radius-width, angle_from, angle_to, right)
+	outer_arc = get_circle_arc(Vector2(0,0), radius+width, angle_from, angle_to, right)
+	
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	
+	# send data on
+	# looks UGLY for some reason even with perfect uvs
+#	var uvs = []
+#
+#	# polygon uvs work differently to 3D uvs
+#	# texture is x 128 y 80
+#
+#	for i in range(inner_arc.size()):
+#		# invert because
+#		uvs.append(Vector2(0,(128.0-(128.0/32)*i)))
+#	for i in range(outer_arc.size()):
+#		uvs.append(Vector2(80, (128.0/32)*i))
+#
+	
+	# clear
+	get_child(0).get_polygon().resize(0)
+	
+	# inner arc is a PoolVector2Array and that one doesn't return a ref
+	#var inner_arc_inv = inner_arc.invert()
+	# polygon's last wants to connect to the first
+	inner_arc.invert()
+	get_child(0).poly = inner_arc + outer_arc
+	#print(get_child(0).poly)
+
+	#get_child(0).set_uv(uvs)
+	get_child(0).set_polygon(get_child(0).poly)
+
+	
+	
+	#pass
+
+
+func _draw():
+	pass
+	#draw_circle_arc(Vector2(0,0), inner_arc, Color(0,0,1))
+	#draw_circle_arc(Vector2(0,0), outer_arc, Color(0,0,1))
+
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
---
+++ straight_better.gd
@@ -0,0 +1,38 @@
+tool
+extends "straight2D.gd"
+
+# class member variables go here, for example:
+export(int) var width = 10
+var inner_first = Vector2(0,0)
+var inner_last = Vector2(0,0)
+var outer_first = Vector2(0,0)
+var outer_last = Vector2(0,0)
+
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	
+	thick = 2
+	
+	inner_first = first + Vector2(0, -width)
+	inner_last = last + Vector2(0, -width)
+	outer_first = first + Vector2(0, width)
+	outer_last = last + Vector2(0, width) 
+	
+	# send data on
+	var poly = [inner_first, inner_last, outer_last, outer_first]
+	
+	get_child(0).set_polygon(poly)
+	
+	
+	#pass
+
+func _draw():
+	draw_line(inner_first, inner_last, Color(0,0,0), thick)
+	draw_line(outer_first, outer_last, Color(0,0,0), thick)
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
