a8669e433b80dffd8d8585f80daa744f25d81592
Zireael07 <zi@mail.com>
2018-12-06 17:08:45 +0100

    new: placer for curve segments

---
+++ Road2D_placer.gd
@@ -0,0 +1,26 @@
+tool
+
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	
+	pass
+
+func place_road():
+	var road = get_child(0)
+		
+	if (road != null):
+		#print("We have a road")
+			
+		var begin = road.first
+		#print(road.get_parent().get_parent().get_name() + " beginning is " + String(begin))
+			
+		var locate = -begin
+
+		road.set_position(locate)
