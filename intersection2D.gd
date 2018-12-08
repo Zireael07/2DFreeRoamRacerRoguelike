23fde6b9f23b39ce3ad9279dc070fda579a88fe9
Zireael07 <zi@mail.com>
2018-12-08 17:13:20 +0100

    new: gfx for intersection

--- intersection2D.gd
+++ intersection2D.gd
@@ -9,10 +9,35 @@ var point_three = Vector2(0,-10*15)
 
 var open_exits = [point_one, point_two, point_three]
 
+# gfx
+export(int) var width = 70
+
+
+
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	pass
+
+	var point_one_in = point_one + Vector2(-width,0)	
+	var point_one_out = point_one + Vector2(width, 0)
+	
+	var point_out_cent = Vector2(width, width)
+	
+	var point_two_out = point_two + Vector2(0, width)
+	var point_two_in = point_two + Vector2(0, -width)
+	
+	var point_two_cent_in = Vector2(width, -width)
+	
+	var point_three_out = point_three + Vector2(width, 0)
+	var point_three_in = point_three + Vector2(-width, 0)
+	
+	# send data on
+	var poly = [point_one_in, point_one_out, point_out_cent, point_two_out, point_two_in, point_two_cent_in, point_three_out, point_three_in]
+	
+	get_child(0).set_polygon(poly)
+	
+	
+	#pass
 
 func _draw():
 	draw_line(Vector2(0,0), point_one, Color(1,0,0))
