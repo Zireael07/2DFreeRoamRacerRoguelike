a490722b6df25138d001b4e5f9258f111ef147db
Zireael07 <zi@mail.com>
2019-03-21 14:38:23 +0100

    automatically connect to first intersection

--- intersections2D_test.gd
+++ intersections2D_test.gd
@@ -8,10 +8,56 @@ extends "intersections2D.gd"
 func _ready():
 	var sorted = sort_intersections_distance()
 	var initial_int = sorted[0][1]
-	print("Initial int" + str(initial_int))
+	print("Initial int: " + str(initial_int))
+
+	# for test purposes
+	var edges = [Vector2(0,1), Vector2(0,2), Vector2(0,3), Vector2(1,3), Vector2(2,3)]
+
+	var next_ints = []
+	var res = []
+	var sorted_n = []
+	for e in edges:
+		if e.x == initial_int:
+			print("Edge with initial int" + str(e) + " other end " + str(e.y))
+			var data = [e.y, get_child(e.y).get_position()]
+			next_ints.append(data)
+			#print(data[1].x)
+			sorted_n.append(data[1].x)
+		if e.y == initial_int:
+			print("Edge with initial int" + str(e) + " other end " + str(e.x))
+			var data = [e.x, get_child(e.x).get_position()]
+			next_ints.append(data)
+			#print(data[1].x)
+			sorted_n.append(data[1].x)
+
+	#print(sorted_n)
+
+	# this sorts by natural order (lower x first)
+	sorted_n.sort()
+	# but we want higher?
+	sorted_n.invert()
 	
-	connect_intersections(1,3)
-	connect_intersections(1,0)
+	print(sorted_n)
+	
+	for i in range(0, next_ints.size()):
+		#print("Attempt " + str(i))
+		for d in next_ints:
+			#print(str(d) + " " + str(sorted_n[0]))
+			if d[1].x == sorted_n[0]:
+				next_ints.remove(next_ints.find(d))
+				res.append(d)
+				sorted_n.remove(0)
+		
+	#print("Res " + str(res) + " lower x: " + str(res[0]))
+	#print("next ints: " + str(next_ints))
+	for i in range(0, res.size()):
+		var p = res[i]
+		print("Intersection " + str(p))
+		connect_intersections(initial_int, p[0])
+	
+	# manual		
+	#connect_intersections(1,3)
+	#connect_intersections(1,0)
 	connect_intersections(0,3)
 	connect_intersections(0,2)
 
@@ -27,10 +73,11 @@ func sort_intersections_distance():
 	var dists = []
 	var tmp = []
 	var closest = []
-	for e in get_children():
+	for i in range(0, get_child_count()):
+		var e = get_child(i)
 		var dist = e.position.distance_to(Vector2(0,0))
 		print("Distance: exit: " + str(e.get_name()) + " dist: " + str(dist))
-		tmp.append([dist, e])
+		tmp.append([dist, i])
 		dists.append(dist)
 
 	dists.sort()
